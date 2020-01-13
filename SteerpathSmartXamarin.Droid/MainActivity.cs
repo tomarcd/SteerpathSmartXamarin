741c4b97ce91b7c8bf6408fb01e6170a9ee77775
tomarcd <tomarcd@gmail.com>
2020-01-13 17:41:02 -0500

    -fixing package name

--- SteerpathSmartXamarin.Droid/MainActivity.cs
+++ SteerpathSmartXamarin.Droid/MainActivity.cs
@@ -6,14 +6,13 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
-using Com.Steerpath.Sdk.Meta;
 using Com.Steerpath.Smart;
-using static Com.Steerpath.Sdk.Meta.MetaQuery;
+
 
 namespace SteerpathSmartXamarin.Droid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity , Com.Steerpath.Sdk.Meta.MetaLoader.ILoadListener
+    public class MainActivity : AppCompatActivity
     {
         readonly string SmartMapFragmentTag = "map";
         protected override void OnCreate(Bundle savedInstanceState)
@@ -37,8 +36,7 @@ namespace SteerpathSmartXamarin.Droid
                 map.AnimateCameraToBuildingRef("341", null);
             }
 
-            MetaLoader.Load((Com.Steerpath.Sdk.Meta.MetaQuery)new MetaQuery.Builder(this, DataType.Buildings).Build(), this);
-
+          
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -69,26 +67,6 @@ namespace SteerpathSmartXamarin.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        public void OnLoaded(MetaQueryResult p0)
-        {
-            if (p0.IsSuccess) {
-                if (p0.MetaFeatures.Count > 0) {
-                    SmartMapFragment map = GetMap();
-                    //if (map != null) {
-                    //    map.NavigateToObject(ToSmartMapObject(p0.MetaFeatures[0]));
-                    //}
-                }
-            }
-        }
-
-        private  SmartMapObject ToSmartMapObject(MetaFeature var0)
-        {
-            SmartMapObject var2 = new SmartMapObject(var0.Latitude, var0.Longitude, var0.FloorIndex, /*var0.LocalRef*/"room", var0.BuildingRef);
-            var2.Title = var0.Title;
-            var2.Source = ObjectSource.Marker;
-            return var2;
         }
     }
 }
